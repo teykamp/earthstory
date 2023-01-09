1bb9fcf68a206938c1738026180e8aaec1580a1f
Thomas Eykamp <embergameslp@gmail.com>
2023-01-09 17:06:42 -0500

    Add Tools to footer

--- src/components/FooterContent.vue
+++ src/components/FooterContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-footer class="pa-6" color="black" dark>
+  <v-footer class="pt-6 pb-6" :color="backgroundColor" dark>
 		<v-row class="justify-center pa-6">
 			<v-btn v-for="social in socials" 
 				:key="social[0]" 
@@ -17,7 +17,18 @@
 		<v-col class="text-center" cols="12">
 			{{ new Date().getFullYear() }} — <strong>tEykamp</strong>
 		</v-col>
-		<v-row class="d-flex justify-end">
+		<v-row class="d-flex justify-space-between align-end">
+			<v-card :color="backgroundColor" dark>
+				<v-row>
+					<div class="d-flex text-overline align-center pl-6">Tools:</div>
+					
+					<v-col v-for="tool in tools" :key="tool[0]">
+						<v-btn icon :href="tool[1]" target="_blank">
+							<v-img :src="tool[0]" max-height="20" max-width="20" contain></v-img>
+						</v-btn>
+					</v-col>
+				</v-row>
+			</v-card>
 			<v-btn
 				x-small
 				dark
@@ -39,6 +50,12 @@ export default {
 				['mdi-github', 'https://github.com/teykamp'],
 				['mdi-xml', 'https://github.com/teykamp/earthstory'],
 			],
+			backgroundColor: 'black',
+			tools: [['https://vue2-leaflet.netlify.app/logo.png', 'https://vue2-leaflet.netlify.app/'], 
+				['https://cdn.vuetifyjs.com/docs/images/logos/vuetify-logo-v3-dark.svg', 'https://next.vuetifyjs.com/en/'], 
+				['https://cdn.freebiesupply.com/logos/large/2x/vue-9-logo-png-transparent.png', 'https://vuejs.org/'], 
+				['https://s3.amazonaws.com/images.seroundtable.com/google-maps-icon-1580992464.png', 'https://www.google.com/maps']
+						],
 		}
 	},
 }
